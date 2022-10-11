d81a44a2a4e774abd26d568f8bee3f3927d3372c
plharraud <plharraud@gmail.com>
2022-10-11 11:41:27 +0200

    unzip code.zip

---
+++ app/src/main/java/bittorrent/client/Torrent.java
@@ -0,0 +1,41 @@
+package bittorrent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import be.adaxisoft.bencode.BDecoder;
+import be.adaxisoft.bencode.BEncodedValue;
+import be.adaxisoft.bencode.BEncoder;
+import be.adaxisoft.bencode.InvalidBEncodingException;
+
+public class Torrent {
+
+	//This class is just an example to bootstrap but you may change everything in it even the name
+	private byte[] info_hash = new byte[20];
+  public Torrent() {
+  }
+	
+	public Torrent(File file) {
+		try {
+			FileInputStream fileInputStream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public byte[] getInfo_hash() {
+		return info_hash;
+	}
+	
+	public String getInfo_hash_hex() {
+		return Utils.bytesToHex(info_hash);
+	}
+
+}
---
+++ app/src/test/java/bittorrent/client/InfoHashTest.java
@@ -0,0 +1,34 @@
+package bittorrent;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+public class InfoHashTest {
+	
+  @Test
+  public void test0() {
+    Torrent t = new Torrent();
+    assertEquals("0000000000000000000000000000000000000000", t.getInfo_hash_hex());
+  }
+	
+	@Test
+	public void testFile1()  {
+		Torrent t = new Torrent(new File("src/main/resources/torrents/mauvais_nom_fichier.txt.torrent")); //mettre hello_world.txt.torrent
+		assertEquals("285DCBB0DC5AE2ECB78F363AD1295A321C8EBFAF", t.getInfo_hash_hex());
+	}
+	
+	@Test
+	public void testFile2()  {
+		Torrent t = new Torrent(new File("src/main/resources/torrents/iceberg.jpg.torrent"));
+		assertEquals("067133ACE5DD0C5027B99DE5D4BA512828208D5B", t.getInfo_hash_hex());
+	}
+	
+	@Test
+	public void testFile3()  {
+		Torrent t = new Torrent(new File("src/main/resources/torrents/test.torrent"));
+		assertEquals("e5f669898038384d240a0f6356582b35ca3181a6".toUpperCase(), t.getInfo_hash_hex());
+	}
+
+}
