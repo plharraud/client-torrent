74dbffc5a23293ed1bda29a79c3fc17d4be4095e
Florent <florent.dobler@gmail.com>
2022-11-17 14:42:15 +0100

    Added comments to The various BittorrentMessage Classes

--- app/src/main/java/bittorrent/client/tcpMessage/BittorrentMessage.java
+++ app/src/main/java/bittorrent/client/tcpMessage/BittorrentMessage.java
@@ -10,24 +10,47 @@ public abstract class BittorrentMessage implements IBittorentMessageBuilder, IBi
     int messageLength;
     int messageType;
 
+    /**
+     * Builds the bittorent message from a DataInputStream
+     * @param dataInput
+     * @throws IOException
+     */
     public BittorrentMessage(DataInputStream dataInput) throws IOException{
         // readInt reads 4 bytes of data and outputs them as an int
-        messageLength = dataInput.readInt();
+        this.messageLength = dataInput.readInt();
         // reads 1 byte of data and output them as an int, if we are at the end of the message , returns -1
-        messageType = dataInput.read();
+        this.messageType = dataInput.read();
+        // Remainder of the message
+        this.dataInput = dataInput;
     }
 
+    /**
+     * Constructor used when the message is build, not decoded
+     * @param messageLength
+     * @param messageType
+     */
     public BittorrentMessage(int messageLength, int messageType) {
         this.messageLength = messageLength;
         this.messageType = messageType;
+        // Explicitely nulls the dataInput
+        this.dataInput = null;
     }
 
+    /**
+     * Copy constructor, usefull when we identify the message
+     * @param bittorrentMessage
+     */
     public BittorrentMessage(BittorrentMessage bittorrentMessage) {
         this.dataInput = bittorrentMessage.dataInput;
         this.messageLength = bittorrentMessage.messageLength;
         this.messageType = bittorrentMessage.messageType;
     }
 
+    /**
+     * Returns an identified and decoded message
+     * @return
+     * @throws IOException
+     */
     public BittorrentMessage identify() throws IOException {
         BittorrentMessageType type = BittorrentMessageType.INT_TO_MESSAGE_TYPE_MAP.get(messageType);
         BittorrentMessage identifiedMessage = null;
